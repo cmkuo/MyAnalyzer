c027f47a2715e2fc878587b726e353d36c476b8d
Rong-Shyang Lu <rong-shyang.lu@cern.ch>
2015-03-23 09:38:00 +0100

    update test.py

--- ESAnalyzer/test/splash/test.py
+++ ESAnalyzer/test/splash/test.py
@@ -7,39 +7,36 @@ process = cms.Process("Demo")
 options = VarParsing.VarParsing ('standard')
 
 # setup any defaults you want
-options.files = 'file:/lookarea_SM/PrivMinidaq.00119656.0001.A.storageManager.00.0000.dat'
+options.files = 'file:/data4/cmkuo/Comissioning2015/run238546_ls0001_streamA_StorageManager.dat'
 options.output = 'test.root'
 options.parseArguments()
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
+#process.load("DQM.Integration.test.FrontierCondition_GT_cfi")
+process.load("DQM.Integration.test.FrontierCondition_GT_Offline_cfi") 
+
 process.maxEvents = cms.untracked.PSet(
-        input = cms.untracked.int32(-1)
+        input = cms.untracked.int32(5)
         )
 
-process.source = cms.Source("PoolSource",
+process.source = cms.Source('NewEventStreamFileReader',
+#process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
     options.files
     #'file:/data3/ncuhep/splash/digi/BeamSplash_1257738510.root'
-    ),
-                            noEventSort = cms.untracked.bool(True),
-                            duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
+    )
                             )
 
-process.ecalPreshowerRecHit = cms.EDProducer("ESRecHitProducer",
-                                             ESrechitCollection = cms.string('EcalRecHitsES'),
-                                             ESdigiCollection = cms.InputTag("ecalPreshowerDigis"),
-                                             algo = cms.string("ESRecHitWorker"),
-                                             ESRecoAlgo = cms.int32(1),
-                                             ESGain = cms.int32(2),
-                                             ESBaseline = cms.int32(0),
-                                             ESMIPADC = cms.double(55),
-                                             ESMIPkeV = cms.double(81.08)
-                                             )
+process.load("EventFilter.ESRawToDigi.esRawToDigi_cfi")
+process.esRawToDigi.sourceTag = cms.InputTag("rawDataCollector")
+process.esRawToDigi.debugMode = False
+
+process.load('RecoLocalCalo.EcalRecProducers.ecalPreshowerRecHit_cfi')
+process.ecalPreshowerRecHit.ESdigiCollection = cms.InputTag("esRawToDigi")
 
 process.splash = cms.EDAnalyzer("ESSplashAnalyzer",
-                                #DigiLabel = cms.InputTag("esRawToDigi"),
-                                DigiLabel = cms.InputTag("ecalPreshowerDigis"),
+                                DigiLabel = cms.InputTag("esRawToDigi"),
                                 RecHitLabel = cms.InputTag("ecalPreshowerRecHit:EcalRecHitsES:Demo"),
                                 EERecHitLabel = cms.InputTag('ecalRecHit:EcalRecHitsEE'),
                                 HERecHitLabel = cms.InputTag('hbhereco'),
@@ -54,10 +51,7 @@ process.splash = cms.EDAnalyzer("ESSplashAnalyzer",
 #                                           )
 #process.out = cms.EndPath(process.patOutputModule)
 
-process.p1 = cms.Path(process.ecalPreshowerRecHit)
-process.p2 = cms.Path(process.splash)
-
-process.schedule = cms.Schedule(process.p1,process.p2)
-
-
+process.p = cms.Path(process.esRawToDigi*
+                     process.ecalPreshowerRecHit*
+                     process.splash)
 
