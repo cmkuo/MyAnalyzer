a368f2f783675e4ffc4465379b8c691976231180
arabella <arabella.martelli@cern.ch>
2015-08-19 14:17:31 +0200

    averagetime per pad

--- ESAnalyzer/test/collisions/InterpretAndPlot/doMappe.C
+++ ESAnalyzer/test/collisions/InterpretAndPlot/doMappe.C
@@ -2,6 +2,108 @@
 #include <iostream>
 
 
+bool Check_list(int i, int j, int k, int m, std::vector<int>& local_k, std::vector<int>& local_m){
+  bool pippo = false;
+  if(i == 0 && j == 0){
+  int sizeList = 49;
+  int zList[49]       = {0,   0, 0,   0,  0,  0,  0,  0,  0,  0,  0   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0};
+  int pList[49]       = {0,   0, 0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0};
+  int kList[49]       = {1,   2, 14, 14, 14, 14, 16, 16, 18, 18, 21, 21, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 35, 38};
+  int mList[49]       = {14, 12, 24, 24, 24, 24, 26, 26, 27, 27, 27, 27,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, 24, 24, 24, 24, 14, 14};
+  int local_kList[49] = {1,   1, 14, 14, 14, 14, 16, 16, 18, 18, 21, 21, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 25, 25, 25, 25, 35, 38};
+  int local_mList[49] = {13, 26, 16, 17, 22, 23, 14, 25, 13, 26, 13, 26, 14, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 16, 17, 22, 23, 13, 13};
+  
+  for(int iC=0; iC<sizeList; ++iC){
+    if(i == zList[iC] && j == pList[iC] && k == kList[iC] && m == mList[iC]){
+      local_k.push_back(local_kList[iC]);
+      local_m.push_back(local_mList[iC]);
+      pippo = true;
+    }
+  }
+  }
+
+  if(i == 0 && j == 1){
+  int sizeList = 52;
+  int zList[52]       = { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0};
+  int pList[52]       = { 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1};
+  int kList[52]       = { 2,  2,  2,  2,  3,  3,  4,  4,  5,  5,  6,  6,  7,  7,  8,  8,  9,  9, 10, 10, 11, 11, 12, 12, 13, 13, 13, 13, 13, 14, 14, 14, 14, 14, 15, 15, 16, 16, 16, 17, 17, 17, 22, 22, 23, 23, 25, 25, 26, 26, 26, 27};
+  int mList[52]       = {10, 10, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27,  2, 22, 22, 27, 27,  1, 24, 24, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 26, 26, 26, 26, 24, 24,  2, 22, 22,  2};
+  int local_kList[52] = { 1,  1,  2,  2,  3,  3,  4,  4,  5,  5,  6,  6,  7,  7,  8,  8,  9,  9, 10, 10, 11, 11, 12, 12, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14, 15, 15, 16, 16, 16, 17, 17, 17, 22, 22, 23, 23, 25, 25, 26, 26, 26, 26};
+  int local_mList[52] = {25, 26, 25, 26, 25, 26, 25, 26, 25, 26, 25, 26, 25, 26, 25, 26, 25, 26, 25, 26, 25, 26, 25, 26,  1, 18, 21, 25, 26, 38, 16, 23, 25, 26, 25, 26, 14, 25, 26, 14, 25, 26, 14, 25, 14, 25, 16, 23,  1, 18, 21, 38};
+  
+  for(int iC=0; iC<sizeList; ++iC){
+    if(i == zList[iC] && j == pList[iC] && k == kList[iC] && m == mList[iC]){
+      local_k.push_back(local_kList[iC]);
+      local_m.push_back(local_mList[iC]);
+      pippo = true;
+    }
+  }
+  }
+
+  if(i == 1 && j == 0){
+  int sizeList = 58;
+  int zList[58]       = { 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1};
+  int pList[58]       = { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0};
+  int kList[58]       = { 1,  2, 14, 14, 14, 14, 16, 16, 18, 18, 21, 21, 23, 23, 23, 24, 25, 25, 25, 25, 26, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 38};
+  int mList[58]       = {14, 12, 24, 24, 24, 24, 26, 26, 27, 27, 27, 27, 26, 26, 33, 33, 24, 24, 24, 24,  6, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 14};
+  int local_kList[58] = { 1,  1, 14, 14, 14, 14, 16, 16, 18, 18, 21, 21, 23, 23, 23, 24, 25, 25, 25, 25, 26, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 38};
+  int local_mList[58] = {13, 26, 16, 17, 22, 23, 14, 25, 13, 26, 13, 26, 14, 25, 32, 32, 16, 17, 22, 23,  5,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 13};
+  
+  for(int iC=0; iC<sizeList; ++iC){
+    if(i == zList[iC] && j == pList[iC] && k == kList[iC] && m == mList[iC]){
+      local_k.push_back(local_kList[iC]);
+      local_m.push_back(local_mList[iC]);
+      pippo = true;
+    }
+  }
+  }
+
+  if(i == 1 && j == 1){
+  int sizeList = 20;
+  int zList[20]       = { 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1};
+  int pList[20]       = { 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1};
+  int kList[20]       = {13, 13, 13, 14, 14, 14, 16, 16, 17, 17, 22, 22, 23, 23, 25, 25, 26, 26, 26, 27};
+  int mList[20]       = { 2, 22, 22,  1, 24, 24, 26, 26, 26, 26, 26, 26, 26, 26, 24, 24,  2, 22, 22,  2};
+  int local_kList[20] = {13, 13, 13, 13, 14, 14, 16, 16, 17, 17, 22, 22, 23, 23, 25, 25, 26, 26, 26, 26};
+  int local_mList[20] = { 1, 18, 21, 38, 16, 23, 14, 25, 14, 25, 14, 25, 14, 25, 16, 23,  1, 18, 21, 38};
+  
+  for(int iC=0; iC<sizeList; ++iC){
+    if(i == zList[iC] && j == pList[iC] && k == kList[iC] && m == mList[iC]){
+      local_k.push_back(local_kList[iC]);
+      local_m.push_back(local_mList[iC]);
+      pippo = true;
+    }
+  }
+  }
+
+  return (pippo);
+
+}
+
+bool Check_Postlist(int i, int j, int k, int m, std::vector<int>& local_k, std::vector<int>& local_m){
+  bool pippo = false;
+  int sizeList = 2;
+  int zList[2]       = {0, 1};
+  int pList[2]       = {0, 0};
+  int kList[2]       = {38, 38};
+  int mList[2]       = {25, 25};
+  int local_kList[2] = {38, 38};
+  int local_mList[2] = {26, 26};
+  
+  for(int iC=0; iC<sizeList; ++iC){
+    if(i == zList[iC] && j == pList[iC] && k == kList[iC] && m == mList[iC]){
+      local_k.push_back(local_kList[iC]);
+      local_m.push_back(local_mList[iC]);
+      pippo = true;
+    }
+  }
+  
+  return (pippo);
+
+}
+
+
+
 //void doMappe()
 void doMappe()
 {
@@ -13,18 +115,24 @@ void doMappe()
   //  TFile *fout = TFile::Open("Ming_timingMap_All.root", "read");
   //  TFile *fout = TFile::Open("Ming_timingMap_AllRuns.root", "read");
   //  TFile *fout = TFile::Open("Ming_timingMap_AllRuns.root", "read");
-  TFile *fout = TFile::Open("Ming_timingMap_AllRuns_245137_246092_MinB.root", "read");
+  TFile *fout = TFile::Open("Run2015_B/doMap_collision2015_RunB_251643_251721_251883_All.root", "read");
 
   TH2F* mapT[2][2];
   TH2F* mapOccupancy[2][2];
   TH2F* mapTrms[2][2];
   TH2F* mapTChi2[2][2];
 
+  int Zval = 1;
+  int Pval = 1;
 
-  //  std::ofstream outFile("outFile.txt", std::ios::out);
+  std::vector<int> local_k;
+  std::vector<int> local_m;
 
-    for (int i=1; i<2; ++i){
-    for (int j=1; j<2; ++j) {
+
+  std::ofstream outFile(Form("timingSensor_RunB_LG_Z%dP%d.txt", Zval, Pval), std::ios::out);
+
+    for (int i=Zval; i<Zval+1; ++i){
+    for (int j=Pval; j<Pval+1; ++j) {
 
       mapT[i][j] = (TH2F*)fout->Get(Form("mapT_Z%d_P%d", i, j));
       mapTrms[i][j]= (TH2F*)fout->Get(Form("mapTrms_Z%d_P%d", i, j));
@@ -35,6 +143,7 @@ void doMappe()
  	for (int m=0; m<40; ++m){
 
 	  if(mapT[i][j]->GetBinContent(k+1, m+1)  != 0.) {
+	    /*
 	    std::cout << i << " \t " << j << " \t " << k << " \t " << m << " \t ";
 	    std::cout.precision(3);
 	    std::cout << mapT[i][j]->GetBinContent(k+1, m+1) << " \t ";  
@@ -44,6 +153,37 @@ void doMappe()
 	    std::cout << mapTChi2[i][j]->GetBinContent(k+1, m+1) << " \t "; 
 	    std::cout.precision(3);
 	    std::cout << mapOccupancy[i][j]->GetBinContent(k+1, m+1) << std::endl;
+	    */
+
+	    local_k.clear();
+	    local_m.clear();
+	    if(Check_list(i, j, k, m, local_k, local_m)){
+	      for(unsigned int iSize=0; iSize<local_k.size(); ++iSize){
+		outFile << i << " \t " << j << " \t " << local_k.at(iSize) << " \t " << local_m.at(iSize) << " \t "; 
+		outFile.precision(3);
+		outFile << -999 << " \t " << -999 << " \t " << -999 << " \t " << -999 << std::endl;
+	      }
+	      local_k.clear();
+	      local_m.clear();
+	    }
+	    outFile << i << " \t " << j << " \t " << k << " \t " << m << " \t "; 
+	    outFile .precision(3);
+	    outFile << mapT[i][j]->GetBinContent(k+1, m+1) << " \t ";  
+	    outFile.precision(3);
+	    outFile << mapTrms[i][j]->GetBinContent(k+1, m+1) << " \t ";   
+	    outFile.precision(3);
+	    outFile << mapTChi2[i][j]->GetBinContent(k+1, m+1) << " \t ";  
+	    outFile.precision(3);
+	    outFile << mapOccupancy[i][j]->GetBinContent(k+1, m+1) << std::endl;         
+	    if(Check_Postlist(i, j, k, m, local_k, local_m)){
+	      for(unsigned int iSize=0; iSize<local_k.size(); ++iSize){
+		outFile << i << " \t " << j << " \t " << local_k.at(iSize) << " \t " << local_m.at(iSize) << " \t "; 
+		outFile.precision(3);
+		outFile << -999 << " \t " << -999 << " \t " << -999 << " \t " << -999 << std::endl;
+	      }
+	      local_k.clear();
+	      local_m.clear();
+	    }
 	  }
 
 	  /*
@@ -89,8 +229,8 @@ void doMappe()
       //      mapT[i][j]->GetZaxis()->SetRangeUser(-20., 20.);
       mapT[i][j]->GetZaxis()->SetRangeUser(-10., 10.);
       mapT[i][j]->Draw("colz");
-      cc->Print(Form("mappe_AllRuns/Run_All_timing_Z%d_P%d.png", i, j), "png");
-      cc->Print(Form("mappe_AllRuns/Run_All_timing_Z%d_P%d.pdf", i, j), "pdf");
+      cc->Print(Form("Run2015_B/mappe_Run2015_B/Run_All_timing_Z%d_P%d.png", i, j), "png");
+      cc->Print(Form("Run2015_B/mappe_Run2015_B/Run_All_timing_Z%d_P%d.pdf", i, j), "pdf");
 
       cc->cd();
       gPad->SetGridx();
@@ -101,16 +241,16 @@ void doMappe()
       mapT[i][j]->GetZaxis()->SetRangeUser(-10., 10.);
       mapT[i][j]->Draw("colz");
       mapT[i][j]->Draw("text, same");
-      cc->Print(Form("mappe_AllRuns/Run_All_timing_text_Z%d_P%d.png", i, j), "png");
-      cc->Print(Form("mappe_AllRuns/Run_All_timing_text_Z%d_P%d.pdf", i, j), "pdf");
+      cc->Print(Form("Run2015_B/mappe_Run2015_B/Run_All_timing_text_Z%d_P%d.png", i, j), "png");
+      cc->Print(Form("Run2015_B/mappe_Run2015_B/Run_All_timing_text_Z%d_P%d.pdf", i, j), "pdf");
       
 
       cc->cd();
       mapOccupancy[i][j]->GetXaxis()->SetTitle("x");
       mapOccupancy[i][j]->GetYaxis()->SetTitle("y");
       mapOccupancy[i][j]->Draw("colz");
-      cc->Print(Form("mappe_AllRuns/Run_All_occupancy_Z%d_P%d.pdf", i, j), "pdf");
-      cc->Print(Form("mappe_AllRuns/Run_All_occupancy_Z%d_P%d.png", i, j), "png");
+      cc->Print(Form("Run2015_B/mappe_Run2015_B/Run_All_occupancy_Z%d_P%d.pdf", i, j), "pdf");
+      cc->Print(Form("Run2015_B/mappe_Run2015_B/Run_All_occupancy_Z%d_P%d.png", i, j), "png");
 
       cc->cd();
       mapTrms[i][j]->GetXaxis()->SetTitle("x");
@@ -118,8 +258,8 @@ void doMappe()
       //      mapT[i][j]->GetZaxis()->SetRangeUser(-20., 20.);
       //      mapTrms[i][j]->GetZaxis()->SetRangeUser(-10., 10.);
       mapTrms[i][j]->Draw("colz");
-      cc->Print(Form("mappe_AllRuns/Run_All_timingRMS_Z%d_P%d.png", i, j), "png");
-      cc->Print(Form("mappe_AllRuns/Run_All_timingRMS_Z%d_P%d.pdf", i, j), "pdf");
+      cc->Print(Form("Run2015_B/mappe_Run2015_B/Run_All_timingRMS_Z%d_P%d.png", i, j), "png");
+      cc->Print(Form("Run2015_B/mappe_Run2015_B/Run_All_timingRMS_Z%d_P%d.pdf", i, j), "pdf");
 
       cc->cd();
       mapTChi2[i][j]->GetXaxis()->SetTitle("x");
@@ -127,10 +267,11 @@ void doMappe()
       //      mapT[i][j]->GetZaxis()->SetRangeUser(-20., 20.);
       mapTChi2[i][j]->GetZaxis()->SetRangeUser(0., 1500.);
       mapTChi2[i][j]->Draw("colz");
-      cc->Print(Form("mappe_AllRuns/Run_All_timingChi2_Z%d_P%d.pdf", i, j), "pdf");
-      cc->Print(Form("mappe_AllRuns/Run_All_timingChi2_Z%d_P%d.png", i, j), "png");
+      cc->Print(Form("Run2015_B/mappe_Run2015_B/Run_All_timingChi2_Z%d_P%d.pdf", i, j), "pdf");
+      cc->Print(Form("Run2015_B/mappe_Run2015_B/Run_All_timingChi2_Z%d_P%d.png", i, j), "png");
     }
-  }
+    }
+    outFile.close();
 
     return;
 
--- ESAnalyzer/test/collisions/InterpretAndPlot/readMappe.cpp
+++ ESAnalyzer/test/collisions/InterpretAndPlot/readMappe.cpp
@@ -1,5 +1,5 @@
 //g++ -Wall -o readMappe `root-config --cflags --glibs` readMappe.cpp
-
+// ./readMappe Zside Pside
 
 #include "TROOT.h"
 #include "TStyle.h"
@@ -72,8 +72,10 @@ int main(int argc, char* argv[])
 
 
   std::ifstream inFileTime;
-  inFileTime.open(Form("SensorLadder_map/timingSensor_Z%dP%d.txt", Zside_i, Pside_i), std::ios::in);
+  //  inFileTime.open(Form("SensorLadder_map/timingSensor_Z%dP%d.txt", Zside_i, Pside_i), std::ios::in);
+  inFileTime.open(Form("SensorLadder_map/timingSensor_RunB_LG_Z%dP%d.txt", Zside_i, Pside_i), std::ios::in);
 
+  int line = 0;
   std::ifstream inFileLong;
   inFileLong.open(Form("SensorLadder_map/outMap_Z%dP%d.txt", Zside_i, Pside_i), std::ios::in);
   while (!inFileLong.eof())
@@ -82,13 +84,14 @@ int main(int argc, char* argv[])
       inFileTime >> iz >> ip >> ix >> iy >> time >> rms >> chi2 >> occ;
       //      std::cout << " ix = " << ix << " iy = " << iy << std::endl;
 
-      if(ix != iX || iy != iY) {
+      if(iZ != iz || iP != ip || ix != iX || iy != iY) {
 	std::cout << " serius problem " << std::endl;	
+	std::cout << " line = " << line << std::endl;
 	std::cout << "iZ = " << iZ << " iP = " << iP << " iX = " << iX << " iY = " << iY << std::endl;
 	std::cout << "iz = " << iz << " ip = " << ip << " ix = " << ix << " iy = " << iy << std::endl;
 	return 100;
       }
-
+      ++line;
       std::pair<int, int> dummyPair(ix, iy);
       sensor_ladder[dummyPair] = iL;
       if(time > -900) {
@@ -100,20 +103,24 @@ int main(int argc, char* argv[])
   inFileTime.close ();
 
   //  std::ifstream inFileLong;
-  inFileTime.open(Form("SensorLadder_map/timingSensor_Z%dP%d.txt", Zside_i, Pside_i), std::ios::in);
+  //  inFileTime.open(Form("SensorLadder_map/timingSensor_Z%dP%d.txt", Zside_i, Pside_i), std::ios::in);
+  inFileTime.open(Form("SensorLadder_map/timingSensor_RunB_LG_Z%dP%d.txt", Zside_i, Pside_i), std::ios::in);
   inFileLong.open(Form("SensorLadder_map/outMap_Z%dP%d.txt", Zside_i, Pside_i), std::ios::in);
   while (!inFileLong.eof())
     {
       inFileLong >> iZ >> iP >> iX >> iY >> iL;
       inFileTime >> iz >> ip >> ix >> iy >> time >> rms >> chi2 >> occ;
-      //      std::cout << " ix = " << ix << " iy = " << iy << std::endl;
+      //std::cout << " ix = " << ix << " iy = " << iy << " t = " << time << " rms = " << rms << " chi2 = " << chi2 << " occ = " << occ << std::endl;
 
 
       if(time > -900 && iL != "058") {
 	map_Z0P0->Fill(iX, iY, lad_time[iL] / lad_N[iL]);
 	map_Z0P0_check->Fill(iX, iY, time);
-	std::cout << " >>> lad_time[iL] / lad_N[iL] = " << lad_time[iL] / lad_N[iL] << std::endl;
-	std::cout << " >>> lad_time[iL] = " << lad_time[iL] << std::endl;
+ 	if(ix == 24 || ix == 23) {
+ 	  std::cout << "iz = " << iz << " ip = " << ip << " ix = " << ix << " iy = " << iy << std::endl;
+ 	  std::cout << " >>> lad_time[iL] / lad_N[iL] = " << lad_time[iL] / lad_N[iL] << std::endl;
+ 	  std::cout << " >>> lad_time[iL] = " << lad_time[iL] << std::endl;
+ 	}
       }
       if(iL == "058") 	{
 	map_Z0P0->Fill(iX, iY, -20.);
@@ -130,7 +137,7 @@ int main(int argc, char* argv[])
   for (int k=0; k<40; ++k){
     for (int m=0; m<40; ++m){
       if(map_Z0P0_check->GetBinContent(k+1, m+1) == 0) {
-	std::cout << " k = " << k  << " m = " << m<< std::endl;
+	//	std::cout << " k = " << k  << " m = " << m<< std::endl;
 	map_Z0P0->SetBinContent(k+1, m+1, -1000000.);
 	map_Z0P0_check->SetBinContent(k+1, m+1, -1000000.);
       }
@@ -150,8 +157,8 @@ int main(int argc, char* argv[])
   //      mapT[i][j]->GetZaxis()->SetRangeUser(-20., 20.);
   map_Z0P0->GetZaxis()->SetRangeUser(-10., 10.);
   map_Z0P0->Draw("colz");
-  cc->Print(Form("mappe_AllRuns/Average_timing_Z%dP%d.png", Zside_i, Pside_i), "png");
-  cc->Print(Form("mappe_AllRuns/Average_timing_Z%dP%d.pdf", Zside_i, Pside_i), "pdf");
+  cc->Print(Form("Run2015_B/mappe_Run2015_B/Average_timing_Z%dP%d.png", Zside_i, Pside_i), "png");
+  cc->Print(Form("Run2015_B/mappe_Run2015_B/Average_timing_Z%dP%d.pdf", Zside_i, Pside_i), "pdf");
 
   //      gPad->SetLogz();
   cc->cd();
@@ -163,8 +170,8 @@ int main(int argc, char* argv[])
   //mapT[i][j]->GetZaxis()->SetRangeUser(-20., 20.);
   map_Z0P0_check->GetZaxis()->SetRangeUser(-10., 10.);
   map_Z0P0_check->Draw("colz");
-  cc->Print(Form("mappe_AllRuns/Average_timing_Z%dP%d_check.png", Zside_i, Pside_i), "png");
-  cc->Print(Form("mappe_AllRuns/Average_timing_Z%dP%d_check.pdf", Zside_i, Pside_i), "pdf");
+  cc->Print(Form("Run2015_B/mappe_Run2015_B/Average_timing_Z%dP%d_check.png", Zside_i, Pside_i), "png");
+  cc->Print(Form("Run2015_B/mappe_Run2015_B/Average_timing_Z%dP%d_check.pdf", Zside_i, Pside_i), "pdf");
  
   return 0;
  
